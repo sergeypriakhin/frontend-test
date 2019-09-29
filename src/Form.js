fed1d359eaa2f5f80e4c64ab3665df2052dadfa5
sergeypriakhin <sergey.priakhin@gmail.com>
2019-09-30 00:29:28 +0300

    first commit

---
+++ src/Form.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './Form.css';
+
+function validateEmail(value) {
+  if (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    return true;
+  }
+  
+  return false;
+}
+
+function Input({ label = null, name, text = 'text', value = '', onChange, error = false }) {
+  return(
+    <>
+      {label && <label htmlFor={name} className='form__label'>{label}</label>}
+      <input
+        id={name}
+        name={name} 
+        type={text} 
+        value={value} 
+        onChange={onChange}
+        className={error ? 'form__input form__input_error' : 'form__input'}
+      /> 
+    </>
+  );
+}
+
+function Form() {
+  const [ value, setValue ] = useState('');
+  const [ error, setError ] = useState(false);
+
+  const handleChange = (e) => {
+    let val = e.target.value;
+    let err = validateEmail(val);
+
+    setError(err);
+    setValue(val)
+  };
+
+  return (
+    <form className='form'>
+      <Input 
+        name='email'
+        type='email'
+        error={error}
+        value={value}
+        label='Email'
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
+
+export default Form;
